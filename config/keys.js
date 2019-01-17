7178d3c50cd6ba1f98da233c63b0d49feea5aeb5
Lushac <lushacn95@gmail.com>
2019-01-17 23:08:42 +0100

    heroku upload

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURL:
-    "mongodb://etablica:etablica@etablica-shard-00-00-3qxgx.mongodb.net:27017,etablica-shard-00-01-3qxgx.mongodb.net:27017,etablica-shard-00-02-3qxgx.mongodb.net:27017/test?ssl=true&replicaSet=eTablica-shard-0&authSource=admin&retryWrites=true",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -35,6 +36,14 @@ require("./config/passport")(passport);
 //Use routes
 app.use("/api/users", users);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
